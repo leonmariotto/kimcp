c836b907f78e63d513489fafdf0f921823ecee2e
Leon Mariotto <leon2mariotto@gmail.com>
2026-01-03 22:05:38 +0100

    Add some experiment with kicad python get_items api

--- kimcp/KiPyWrap.py
+++ kimcp/KiPyWrap.py
@@ -6,6 +6,8 @@ from kipy.board_types import BoardLayer, Zone
 from kipy.common_types import PolygonWithHoles
 from kipy.geometry import PolyLine, PolyLineNode
 from kipy.util import from_mm
+from kipy.proto.common.types import KiCadObjectType
+from kipy.proto.common.types import DocumentType
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -14,8 +16,46 @@ logging.basicConfig(
 
 
 class KiPyWrap:
+    """
+    """
     def __init__(self):
         self.kicad = kipy.KiCad()
+        self.board = self.kicad.get_board()
+
+    def get_board_info(self):
+        """
+        The main API here is get_items, it take a list of id in parameters to filter which item
+        it return.
+        Here's the list of types :
+            - KiCadObjectType.KOT_UNKNOWN
+		    - KiCadObjectType.KOT_PCB_FOOTPRINT
+		    - KiCadObjectType.KOT_PCB_PAD
+		    - KiCadObjectType.KOT_PCB_SHAPE
+		    - KiCadObjectType.KOT_PCB_REFERENCE_IMAGE
+		    - KiCadObjectType.KOT_PCB_FIELD
+		    - KiCadObjectType.KOT_PCB_GENERATOR
+		    - KiCadObjectType.KOT_PCB_TEXT
+		    - KiCadObjectType.KOT_PCB_TEXTBOX
+		    - KiCadObjectType.KOT_PCB_TABLE
+		    - KiCadObjectType.KOT_PCB_TABLECELL
+		    - KiCadObjectType.KOT_PCB_TRACE
+		    - KiCadObjectType.KOT_PCB_VIA
+		    - KiCadObjectType.KOT_PCB_ARC
+		    - KiCadObjectType.KOT_PCB_MARKER
+		    - KiCadObjectType.KOT_PCB_DIMENSION
+		    - KiCadObjectType.KOT_PCB_ZONE
+		    - KiCadObjectType.KOT_PCB_GROUP
+        Obtain the full list by running :
+        print([name for name in KiCadObjectType.keys()])
+        """
+        board = self.kicad.get_board()
+        logging.debug("Board={%s}", str(board))
+        logging.debug("Copper layers={%s}", str(board.get_copper_layer_count()))
+        logging.debug("Footprint list={%s}", str(board.get_footprints()))
+        logging.debug("Trace list={%s}", str(board.get_items(types=[KiCadObjectType.KOT_PCB_TRACE])))
+        #logging.debug("Items list={%s}", str(board.get_items()))
+        # logging.debug("Dimensions={%s}", str(board.get_dimensions()))
+        # kipy.errors.ApiError: KiCad returned error: none of the requested types are valid for a Board object
 
     def draw_zone(self):
         board = self.kicad.get_board()
@@ -42,4 +82,4 @@ class KiPyWrap:
 def test_kicad():
     # Pass lifespan to server
     kiwrap = KiPyWrap()
-    kiwrap.check_version()
+    kiwrap.get_board_info()
